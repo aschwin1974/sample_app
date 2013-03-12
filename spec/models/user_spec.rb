b8c4ae1bcdc2d89698617533ff6112e1df126ea8
Adam Schwindt <adam@zyantus.com>
2013-03-12 14:50:05 -0500

    little laptop

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,10 +25,16 @@ before do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-  it { should respond_to(:authenticate) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }  
 
 
   it { should be_valid }
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
   describe "when email is not present" do
     before { @user.email = " " }
@@ -109,6 +115,7 @@ describe "email address with mixed case" do
       @user.reload.email.should == mixed_case_email.downcase
     end
   end
+
 
 end
 end
